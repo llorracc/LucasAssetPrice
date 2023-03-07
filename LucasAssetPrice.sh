078efa773f1ef296e046cdf9a39ed9eeb034ee4d
llorracc <git@llorracc.org>
2023-03-06 23:54:31 -0500

    Compiled whole thing

--- LucasAssetPrice.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-make4ht --utf8 --config LucasAssetPrice.cfg --format html5 LucasAssetPrice "svg" "-cunihtf -utf8"
-cat page-style.css | cat - LucasAssetPrice-generated-by-make4ht.css > LucasAssetPrice-page-style.css && mv LucasAssetPrice-page-style.css LucasAssetPrice.css
-cp LucasAssetPrice.html index.html
