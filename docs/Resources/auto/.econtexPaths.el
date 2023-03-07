f7db7c8721af95dc966a474795cdd50464988573
llorracc <git@llorracc.org>
2023-03-06 23:41:10 -0500

    Restore docs

---
+++ docs/Resources/auto/.econtexPaths.el
@@ -0,0 +1,27 @@
+(TeX-add-style-hook
+ ".econtexPaths"
+ (lambda ()
+   (TeX-add-symbols
+    "packages"
+    "classes"
+    "econark"
+    "econtexSetup"
+    "econtexShortcuts"
+    "econtexBibMake"
+    "econtexBibStyle"
+    "econtexBib"
+    "handout"
+    "handoutSetup"
+    "handoutShortcuts"
+    "handoutBibMake"
+    "handoutBibStyle"
+    "FigDir"
+    "CodeDir"
+    "DataDir"
+    "SlideDir"
+    "TableDir"
+    "ApndxDir"
+    "EqDir"
+    "LaTeXInputs"))
+ :latex)
+
